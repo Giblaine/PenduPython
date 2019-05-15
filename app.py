36ec4cba3d9d2744d61addd5d69ce7586ad766b4
Clement <clement.gourcuff@gmail.com>
2019-05-15 11:37:13 +0200

    Images

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import requests
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_url_path='/static')
 
 @app.route('/')
 def index():
